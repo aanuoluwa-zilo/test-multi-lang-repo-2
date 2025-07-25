5b68dcb76fc33f5b62561232545b6ae4fa8320ba
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-25 16:37:16 +0100

    Add src/csharp/Calculator.cs

---
+++ src/csharp/Calculator.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace MultiLangApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
