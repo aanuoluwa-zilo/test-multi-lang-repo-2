c3592da72d421ae15c7a0309c9abac9aa58bd3cf
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-25 16:37:18 +0100

    Add src/csharp/Models/Config.cs

---
+++ src/csharp/Models/Config.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MultiLangApp.Models
+{
+    public class Config
+    {
+        public bool Debug { get; set; }
+        public string Version { get; set; }
+        public int Port { get; set; }
+    }
+}
