20e1404190d495cf1cfe409ce8b99ac639f12ee8
aanuoluwa-zilo <168453615+aanuoluwa-zilo@users.noreply.github.com>
2025-07-28 11:24:52 +0100

    Add Unicode file: src/go/测试文件.go

---
+++ src/go/测试文件.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 测试函数
+// Test function
+func 测试函数() {
+	fmt.Println("Hello from Unicode function!")
+}
+
+func main() {
+	fmt.Println("Hello from Go!")
+	测试函数()
+}
